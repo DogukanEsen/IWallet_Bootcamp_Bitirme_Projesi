aa75f534671357a54f60a8463282395052238a48
Satı Doğukan ESEN <esendogukan@hotmail.com>
2024-06-21 10:50:29 +0300

    albums_controller test added

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -4,4 +4,15 @@ class AlbumsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @album = albums(:one)  # Örnek bir album fixture'ı
+  end
+
+  test "should show album" do
+    get album_url(@album)
+    assert_response :success
+    assert_not_nil assigns(:album)
+    assert_not_nil assigns(:album_details)
+  end
 end
